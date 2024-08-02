0c12cb7cd8d42975d90df5c85acc8bc18afd1626
Chris Ryan <pkchris@gmail.com>
2024-08-01 23:33:00 -0400

    Zip contents of build directory at end of build process and temporarily disable JS minification

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ var gulp = require('gulp'),
   browserify = require('browserify'),
   uglify = require('gulp-uglify'),
   streamify = require('gulp-streamify'),
-  babelify = require("babelify");
-  gsap = require("gsap");
-  glslify = require("glslify");
+  babelify = require("babelify"),
+  gsap = require("gsap"),
+  glslify = require("glslify"),
+  zip = require('gulp-zip');
 
 function compileJS(file){
   browserify('src/'+file+'.js',{debug:true})
@@ -14,11 +15,19 @@ function compileJS(file){
     .transform('glslify')
     .bundle()
     .on("error", function (err) { console.log("Error : " + err.message); })
-    .pipe(source(file+'.min.js'))
-    .pipe(streamify(uglify()))
+    .pipe(source(file+'.js'))
+    //.pipe(streamify(uglify()))
     .pipe(gulp.dest('build/js'));
 }
-gulp.task('default',['js'],function(){});
-gulp.task('js',function(){
+
+gulp.task('default', ['js', 'zip'], function(){});
+
+gulp.task('js', function(){
   compileJS('index');
+});
+
+gulp.task('zip', function(){
+  return gulp.src('build/**/*')
+    .pipe(zip('build.zip'))
+    .pipe(gulp.dest('.'));
 });
